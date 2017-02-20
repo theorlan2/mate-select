df68837e5b52b4321a46b0b8bb7ca27533b1475c
theorlan2 <theorlan2@gmail.com>
2017-02-20 11:04:18 -0400

    arreglos de pagina web y de estilos en el select, estilo android al ejecuar en celular 2

--- src/js/mate-select.js
+++ src/js/mate-select.js
@@ -1,9 +1,12 @@
-window.onload = function(){
-  
-  crear_select();
-}
 
-var li = new Array();
+// Validar cual navegador esta utilizando 
+var Navegador_ = (window.navigator.userAgent||window.navigator.vendor||window.opera),
+		Firfx = /Firefox/i.test(Navegador_),
+		Mobile_ = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(Navegador_),
+	FirfoxMobile = (Firfx && Mobile_);
+
+
+var $elect_li = new Array();
 function crear_select(){
 var div_cont_select = document.querySelectorAll("[data-mate-select='active']");
 var select_ = '';
@@ -12,25 +15,33 @@ div_cont_select[e].setAttribute('data-indx-select',e);
 div_cont_select[e].setAttribute('data-selec-open','false');
 var ul_cont = document.querySelectorAll("[data-indx-select='"+e+"'] > .cont_list_select_mate > ul");
  select_ = document.querySelectorAll("[data-indx-select='"+e+"'] >select")[0];
+ if (Mobile_ || FirfoxMobile) { 
+select_.addEventListener('change', function () {
+ _select_option(select_.selectedIndex,e);
+});
+ }
 var select_optiones = select_.options;
 document.querySelectorAll("[data-indx-select='"+e+"']  > .selecionado_opcion ")[0].setAttribute('data-n-select',e);
+document.querySelectorAll("[data-indx-select='"+e+"']  > .icon_select_mate ")[0].setAttribute('data-n-select',e);
 for (var i = 0; i < select_optiones.length; i++) {
-li[i] = document.createElement('li');
+$elect_li[i] = document.createElement('li');
 if (select_optiones[i].selected == true || select_.value == select_optiones[i].innerHTML ) {
-li[i].className = 'active';
+$elect_li[i].className = 'active';
 document.querySelector("[data-indx-select='"+e+"']  > .selecionado_opcion ").innerHTML = select_optiones[i].innerHTML;
 };
-li[i].setAttribute('data-index',i);
-li[i].setAttribute('data-selec-index',e);
+$elect_li[i].setAttribute('data-index',i);
+$elect_li[i].setAttribute('data-selec-index',e);
 // funcion click al selecionar 
-li[i].addEventListener( 'click', function(){  _select_option(this.getAttribute('data-index'),this.getAttribute('data-selec-index')); });
+$elect_li[i].addEventListener( 'click', function(){  _select_option(this.getAttribute('data-index'),this.getAttribute('data-selec-index')); });
 
-li[i].innerHTML = select_optiones[i].innerHTML;
-ul_cont[0].appendChild(li[i]);
+$elect_li[i].innerHTML = select_optiones[i].innerHTML;
+ul_cont[0].appendChild($elect_li[i]);
 
     }; // Fin For select_optiones
   }; // fin for divs_cont_select
 } // Fin Function 
+
+
 
 var cont_slc = 0;
 function open_select(idx){
@@ -38,7 +49,16 @@ var idx1 =  idx.getAttribute('data-n-select');
   var ul_cont_li = document.querySelectorAll("[data-indx-select='"+idx1+"'] .cont_select_int > li");
 var hg = 0;
 var slect_open = document.querySelectorAll("[data-indx-select='"+idx1+"']")[0].getAttribute('data-selec-open');
-
+var slect_element_open = document.querySelectorAll("[data-indx-select='"+idx1+"'] select")[0];
+ if (Mobile_ || FirfoxMobile) { 
+  if (window.document.createEvent) { // All
+  var evt = window.document.createEvent("MouseEvents");
+  evt.initMouseEvent("mousedown", false, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+	slect_element_open.dispatchEvent(evt);
+} else if (slect_element_open.fireEvent) { // IE
+  slect_element_open.fireEvent("onmousedown");
+}
+}else {  
   for (var i = 0; i < ul_cont_li.length; i++) {
 hg += ul_cont_li[i].offsetHeight;
 }; 
@@ -51,6 +71,7 @@ hg += ul_cont_li[i].offsetHeight;
  document.querySelectorAll("[data-indx-select='"+idx1+"'] > .icon_select_mate")[0].style.transform = 'rotate(0deg)';
  document.querySelectorAll("[data-indx-select='"+idx1+"'] > .cont_list_select_mate > ul")[0].style.height = "0px";
  }
+}
 
 } // fin function open_select
 
@@ -63,6 +84,9 @@ document.querySelector("[data-indx-select='"+indx+"'] > .icon_select_mate").styl
 
 
 function _select_option(indx,selc){
+ if (Mobile_ || FirfoxMobile) { 
+selc = selc -1;
+}
   var li_s = document.querySelectorAll("[data-indx-select='"+selc+"'] .cont_select_int > li");
   var p_act = document.querySelectorAll("[data-indx-select='"+selc+"'] > .selecionado_opcion")[0].innerHTML = li_s[indx].innerHTML;
 var select_optiones = document.querySelectorAll("[data-indx-select='"+selc+"'] > select > option");
